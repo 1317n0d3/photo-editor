19125c78494d919953ad42b5cd208e63d9a3b9da
1317n0d3 <kostya.svetashov.01@gmail.com>
2024-03-01 17:13:47 +0400

    add opencv.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,21 @@
-const width = 640;
-const height = 420;
-
-const canvas = document.getElementById("canvas");
-canvas.width = width;
-canvas.height = height;
-
-const ctx = canvas.getContext("2d");
-const newImage = document.getElementById("newImage");
-
-const img = new Image();
-img.src = "./src/images/sample.jpg";
-img.onload = function () {
-  console.log(this.width, this.height);
-  ctx.drawImage(img, 0, 0, width, height);
+let imgElement = document.getElementById("imageSrc");
+let inputElement = document.getElementById("fileInput");
+inputElement.addEventListener(
+  "change",
+  (e) => {
+    imgElement.src = URL.createObjectURL(e.target.files[0]);
+  },
+  false
+);
+imgElement.onload = function () {
+  let mat = cv.imread(imgElement);
+  cv.imshow("canvasOutput", mat);
+  mat.delete();
 };
 
-function previewFile() {
-  const file = document.querySelector("input[type=file]").files[0];
-  const reader = new FileReader();
-
-  reader.onloadend = function () {
-    img.src = reader.result;
-  };
-
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    img.src = "";
-  }
-}
-
-newImage.addEventListener("change", previewFile);
+const Module = {
+  // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
+  onRuntimeInitialized() {
+    document.getElementById("status").innerHTML = "OpenCV.js is ready.";
+  },
+};
