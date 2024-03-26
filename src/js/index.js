0a8ec5b696c85862f67b522542434f074ddab39b
1317n0d3 <kostya.svetashov.01@gmail.com>
2024-03-26 20:56:12 +0400

    add some metadata of image

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 let imgElement = document.getElementById("imageSrc");
 let inputElement = document.getElementById("fileInput");
+let canvas = document.getElementById("canvasOutput");
+const ctx = canvas.getContext("2d");
 
 let fileSizeInKb = 0;
+let fileType = "unknown type";
+let colorDepth = 0;
+let colorScheme = "unknown scheme";
 
 inputElement.addEventListener(
   "change",
@@ -9,6 +14,7 @@ inputElement.addEventListener(
     const file = e.target.files[0];
     imgElement.src = URL.createObjectURL(file);
 
+    fileType = file.type.split("/")[1];
     fileSizeInKb = Math.round(file.size / 1024);
   },
   false
@@ -16,9 +22,27 @@ inputElement.addEventListener(
 
 imgElement.onload = function () {
   let mat = cv.imread(imgElement);
-  setImageInfo(imgElement, fileSizeInKb);
+
+  console.log(mat.channels());
 
   cv.imshow("canvasOutput", mat);
+
+  const imgData = ctx.getImageData(
+    0,
+    0,
+    imgElement.naturalWidth,
+    imgElement.naturalHeight
+  );
+
+  console.log(imgData.data);
+
+  console.log(imgData.data.BYTES_PER_ELEMENT * 8);
+
+  console.log(
+    imgData.data.length / (imgElement.naturalWidth * imgElement.naturalHeight)
+  );
+
+  setImageInfo(imgElement, fileSizeInKb, colorDepth, fileType, colorScheme);
 
   mat.delete();
 };
--- src/js/setImageInfo.js
+++ src/js/setImageInfo.js
@@ -4,13 +4,13 @@ const infoColorDepth = document.querySelector("#infoColorDepth");
 const infoFileFormat = document.querySelector("#infoFileFormat");
 const infoColorScheme = document.querySelector("#infoColorScheme");
 
-const setImageInfo = (imgElement, size) => {
+const setImageInfo = (imgElement, size, depth, type, scheme) => {
   let width = imgElement.naturalWidth;
   let height = imgElement.naturalHeight;
 
   infoSize.innerHTML = `Size: ${size}kb`;
   infoResolution.innerHTML = `Resolution: ${width} x ${height}`;
-  infoColorDepth.innerHTML = `Color depth: `;
-  infoFileFormat.innerHTML = `File format: `;
-  infoColorScheme.innerHTML = `Color scheme: `;
+  infoColorDepth.innerHTML = `Color depth: ${depth} bit`;
+  infoFileFormat.innerHTML = `File format: ${type}`;
+  infoColorScheme.innerHTML = `Color scheme: ${scheme}`;
 };
